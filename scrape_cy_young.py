488051b476ce7abe1c10b7159dbfdd133b76f042
Jaclyn Rich <jaclyn.r.rich@gmail.com>
2018-02-05 23:16:31 -0500

    feat: scrape for both leagues and 2008-2017

--- scrape_cy_young.py
+++ scrape_cy_young.py
@@ -10,25 +10,37 @@ import urllib.request, urllib.parse, urllib.error
 from bs4 import BeautifulSoup
 
 #%%
-url = 'http://www.baseball-reference.com/awards/awards_2008.shtml'
-html = urllib.request.urlopen(url).read()
-soup = BeautifulSoup(html, 'lxml')
+years = ['2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015',
+         '2016', '2017']
+BASE_URL = 'http://www.baseball-reference.com/awards/awards_'
+END_URL = '.shtml'
+
+awards = ['all_AL_CYA_voting', 'all_NL_CYA_voting']
+leagues = ['AL', 'NL']
 
 finalists = []
-div = soup.find('div', id='all_AL_CYA_voting')
-
-# Table is within comment
-al = div.find('div', class_='placeholder').next_sibling.next_sibling
-
-new_soup = BeautifulSoup(al, 'lxml')
-body = new_soup.find('tbody')
-for tr in body.findAll('tr'):
-    info = {}
-    info['rank'] = tr.find('th', class_='right').text
-    info['name'] = tr.find('a').text
-    td = tr.find('td').next_sibling.next_sibling
-    info['points_won'] = td.text
-    info['votes_first'] = td.next_sibling.text
-    info['share'] = td.next_sibling.next_sibling.text[:-1]
+for year in years:
+    url = BASE_URL + year + END_URL
+    html = urllib.request.urlopen(url).read()
+    soup = BeautifulSoup(html, 'lxml')
     
-    finalists.append(info)
+    for ix, award in enumerate(awards):
+        div = soup.find('div', id=award)
+        
+        # Table is within comment
+        al = div.find('div', class_='placeholder').next_sibling.next_sibling
+        
+        new_soup = BeautifulSoup(al, 'lxml')
+        body = new_soup.find('tbody')
+        for tr in body.findAll('tr'):
+            info = {}
+            info['year'] = year
+            info['league'] = leagues[ix]
+            info['rank'] = tr.find('th', class_='right').text
+            info['name'] = tr.find('a').text
+            td = tr.find('td').next_sibling.next_sibling
+            info['points_won'] = td.text
+            info['votes_first'] = td.next_sibling.text
+            info['share'] = td.next_sibling.next_sibling.text[:-1]
+            
+            finalists.append(info)
