2dca657dbe0bea76b5cfe87d12b1f6408fc38c50
Jaclyn Rich <jaclyn.r.rich@gmail.com>
2018-02-07 00:31:01 -0500

    feat: scrape from all urls and write out to csv

--- scrape_pitchfx.py
+++ scrape_pitchfx.py
@@ -8,7 +8,9 @@ Created on Tue Feb  6 22:35:58 2018
 """
 import urllib.request, urllib.parse, urllib.error
 from bs4 import BeautifulSoup
+import pandas as pd
 
+#%%
 years = ['2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015',
          '2016', '2017']
 
@@ -16,8 +18,10 @@ pitch_types = ['FA', 'SI', 'FC', 'CU', 'SL', 'CH', 'FS', 'SB', 'KN']
 long_names = ['fourseam', 'sinker', 'cutter', 'curve', 'slider', 'change',
               'splitter', 'screwball', 'knuckleball']
 
+stats = []
 for year in years:
     for pitch in pitch_types:
+        # scrape urls for each year and pitch type
         url_pieces = ['https://legacy.baseballprospectus.com/pitchfx/',
                       'leaderboards/index.php?hand=&reportType=pfx&prp=P',
                       '&month=&year=', year, '&pitch=', pitch, 
@@ -26,36 +30,32 @@ for year in years:
         
         html = urllib.request.urlopen(url).read()
         soup = BeautifulSoup(html, 'lxml')
-        table = soup.findAll('tbody')[1]
-
+        table = soup.find('tbody')
+        
+        for tr in table.findAll('tr'):
+            
+            tds = tr.findAll('td')
+        
+            info = {}
+            info['year'] = year
+            info['pitch_type'] = pitch
+            info['name'] = tds[1].text
+            info['throws'] = tds[3].text
+            info['num_pitches'] = tds[4].text
+            info['velocity'] = tds[5].text
+            info['H_movement'] = tds[6].text
+            info['V_movement'] = tds[7].text
+            info['swing_rate'] = tds[8].text[:-1]
+            info['whiff/swing'] = tds[9].text[:-1]
+            info['foul/swing'] = tds[10].text[:-1]
+            info['GB/FB'] = tds[11].text
+            info['GB/BIP'] = tds[12].text[:-1]
+            info['LD/BIP'] = tds[13].text[:-1]
+            info['FB/BIP'] = tds[14].text[:-1]
+            info['PU/BIP'] = tds[15].text[:-1]
+            
+            stats.append(info)
 
 #%%
-html = urllib.request.urlopen(url).read()
-soup = BeautifulSoup(html, 'lxml')
-table = soup.find('tbody')
-tds = table.findAll('td')
-
-stats = []
-
-trs = table.findAll('tr')
-
-for tr in table.findAll('tr'):
-    tds = tr.findAll('td')
-
-    info = {}
-    info['name'] = tds[1].text
-    info['throws'] = tds[3].text
-    info['num_pitches'] = tds[4].text
-    info['velocity'] = tds[5].text
-    info['H_movement'] = tds[6].text
-    info['V_movement'] = tds[7].text
-    info['swing_rate'] = tds[8].text[:-1]
-    info['whiff/swing'] = tds[9].text[:-1]
-    info['foul/swing'] = tds[10].text[:-1]
-    info['GB/FB'] = tds[11].text
-    info['GB/BIP'] = tds[12].text[:-1]
-    info['LD/BIP'] = tds[13].text[:-1]
-    info['FB/BIP'] = tds[14].text[:-1]
-    info['PU/BIP'] = tds[15].text[:-1]
-    
-    stats.append(info)
+df = pd.DataFrame(stats)
+df.to_csv('baseball_prospectus_pitchfx.csv', index=False)
